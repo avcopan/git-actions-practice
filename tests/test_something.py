f6d7f9ed829bbb20971b8d0942e8ff0dd4e83356
Andreas Copan <avcopan@gmail.com>
2024-05-28 16:01:28 -0400

    Demo project

---
+++ tests/test_something.py
@@ -0,0 +1,10 @@
+def test__something():
+    """test something"""
+    print("Testing something")
+    assert True, "This didn't work"
+
+
+def test__something_else():
+    """test something"""
+    print("Testing something else")
+    assert True, "This didn't work"
